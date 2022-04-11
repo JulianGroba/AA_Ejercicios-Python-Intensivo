7eb5d850f9d8632642e701997bbb2728fb1f72c5
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-11 17:05:01 +0200

    Ejercicio 4

---
+++ Ejercicio 4 saludador y edad.py
@@ -0,0 +1,6 @@
+nombre = input ("Dime tu nombre: ")
+edad = int(input("¿Cuantos años tienes? "))
+if edad >= 18:
+    print("Hola "+nombre +", eres mayor de edad")
+else:
+    print("Hola "+nombre +", eres menor de edad")
---
+++ Ejercicio 4 saludador y edad_reducida.py
@@ -0,0 +1,4 @@
+if int(input("¿Cuantos años tienes? ")) >= 18:
+    print("Hola "+input ("Dime tu nombre: ") +", eres mayor de edad")
+else:
+    print("Hola "+input ("Dime tu nombre: ") +", eres menor de edad")
