ae872d047edee2dc95108ce79f67c35c39d572b5
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-11 16:40:30 +0200

    Ejercicio 2 y 3

---
+++ Ejercicio 2 Saludador.py
@@ -0,0 +1,5 @@
+nombre = input("Dime tu nombre:")
+if nombre =="Beaver":
+    print("Hola, " +nombre +" ¿Qué tal la tarde?")
+else:
+    print("Grande Moo")
---
+++ Ejercicio 3 Edad.py
@@ -0,0 +1,5 @@
+edad = float (input("¿Cuantos años tienes?"))
+if edad > 17:
+    print("Hola, eres mayor de edad")
+else:
+    print("Tienes 17 años o menos")
