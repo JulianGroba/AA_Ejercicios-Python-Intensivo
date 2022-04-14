5f5416b0cef4266cf666bc6d6c8c86b244ed440d
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-14 13:31:01 +0200

    Update 013 buscasdor de matrícula.py

--- 013 buscasdor de matrícula.py
+++ 013 buscasdor de matrícula.py
@@ -3,7 +3,14 @@ SV650A = ("Suzuki", "SV650A", "Negro")
 Micra = ("Nissan", "Micra", "Cobre")
 diccionario ={"4379-GKB":C4, "3701-KFH": SV650A, "8920-GBR": Micra}
 matricula = input("Indicar matrícula a buscar: ")
+try:
+    vehiculo_buscado=diccionario[matricula]
+    print(vehiculo_buscado)
+except:
+    print("El vehículo no existe.")
+"""
 if matricula in diccionario:
     print(diccionario[matricula])
 else:
     print("Matrícula no incluida en base de datos.")
+    """
