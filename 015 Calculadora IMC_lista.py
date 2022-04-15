66a74a3bf571d0200b97e91e856cc8c78c91e879
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-15 12:19:17 +0200

    Create 015 Calculadora IMC_lista.py

---
+++ 015 Calculadora IMC_lista.py
@@ -0,0 +1,24 @@
+referencias = [
+    [0,"Delgadez muy severa"],
+    [15,"Delgadez severa"],
+    [16,"Delgadez"], 
+    [18.5,"Peso Saludable"], 
+    [25,"Sobrepeso"],
+    [30,"Obesidad Moderada"], 
+    [35,"Obesidad severa"], 
+    [40,"Obesidad muy severa"]]
+referencias.reverse()
+
+
+altura = float (input("Introduce tu altura en metros:"))
+peso = float (input("Introduce tu peso en kg:"))
+imc = peso/ altura**2
+print("%.2f" %imc)#redondear a dos decimales
+rango = ""
+
+for ref in referencias:
+    if (imc>=ref[0]):
+        rango = ref[1]
+        break
+
+print("Tu estado es:",rango)
