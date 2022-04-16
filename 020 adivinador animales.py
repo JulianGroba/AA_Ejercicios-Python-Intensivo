41a017afe99e315c3837310e13684f9be0333ecc
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-16 12:52:32 +0200

    020 y 021

---
+++ 020 adivinador animales.py
@@ -0,0 +1,11 @@
+animals = ["Dog", "Cat", "Bird", "Cow", "Snake"]
+animal = input("Introduce un animal en inglés:")
+numError = 0
+#asegurar que todo será en mayúsculas.
+animals_upper = [x.upper() for x in animals]
+while animal.upper() not in animals_upper:
+    numError+=1
+    print("No incluido en lista")
+    animal = input("Introduce un animal en inglés:")
+
+print("Has acertado, con el siguiente número de errores:", numError)
