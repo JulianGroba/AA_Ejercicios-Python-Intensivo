0deedda8762cc21a07570351d0dd80382df830a2
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-04-16 19:07:32 +0200

    Update 023 buscador dia semana.py

--- 023 buscador dia semana.py
+++ 023 buscador dia semana.py
@@ -1,10 +1,12 @@
 dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
 print(dias)
-dia_buscado = input("Introducir un día vde la semana:")
+dia_buscado = input("Introducir un día de la semana:")
 
 for dia in dias:
     if dia_buscado == dia:
-        print("El día buscado es", dia.capitalize())
+        print("El día buscado es", dia.capitalize()+".")
         break
+    else:
+        print(dia.capitalize(),"no es el día buscado.")
 
-print ("Ejercicio resuelto")
+print ("Ejercicio resuelto.")
