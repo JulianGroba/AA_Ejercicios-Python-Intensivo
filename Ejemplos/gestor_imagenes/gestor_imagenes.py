8e78e830ce1773e6c55fb4f914c51c85bea0b950
JulianGroba <103415331+JulianGroba@users.noreply.github.com>
2022-05-01 20:17:10 +0200

    Create gestor_imagenes.py

---
+++ Ejemplos/gestor_imagenes/gestor_imagenes.py
@@ -0,0 +1,10 @@
+#from groba import descargador_imagenes
+#import groba.descargador_imagenes
+import groba.descargador_imagenes as di
+
+url = input("Introduce la URL del fichero gráfico:")
+nombre_fichero = input("Introduce el nombre del fichero con su extensión:")
+
+#paniagua.descargador_imagenes.get_image_from_url(url,nombre_fichero)
+di.get_image_from_url(url, nombre_fichero)
+
