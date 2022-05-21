84a128c2f8f90355a1b1da5f2af3d1102d02320c
Groba <103415331+JulianGroba@users.noreply.github.com>
2022-05-21 19:41:39 +0200

    Create PracticaFinal.py
    Empezar el ejercicio final.

---
+++ AA_Practica_Final/PracticaFinal.py
@@ -0,0 +1,23 @@
+"""
+Ejercicio Final
+
+1) Un programa que genere un fichero con "n" registros compuestos por nombres y valores aleatorios.
+    Los nombres deben ser de una lista de 10.
+    Los valores de un rango entre 0 y n. El valor "n" debe ser solicitado al usuario mediante teclado.
+    Usar Filas y Tuplas.
+2) Un programa que solicite un nombre al usario y muestre el valor asociado, mediante el uso de un mapa.
+3) Un programa que calcule la suma, la media, el valor máximo y minimo de los "n" números incluidos en el fichero.
+4) Un programa que muestre el listado de la siguiente forma.
+    -Ordenados Alfaéticamente.
+    -Todos los nombres en Mayusculas.
+    -Nombres a la izquierda y valores a la derecha.
+    -Letras A remplezadas por @
+    -Sólo los individuos con valores superiores o iguales a 1000.
+
+El código debe estar estructurado en funciones.
+Controlar errores en los accesos a ficheros.
+Incluir dos pruebas unitarias.
+Incluir un manual de instrucciones de los diferentes programas.
+
+
+"""
